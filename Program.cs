ce8fabd57a415263eecfbe74bb1c251277024bbb
sam1raem <samiramoradi2001@gmail.com>
2024-02-24 11:38:08 +0100

    ändring i kod

--- Program.cs
+++ Program.cs
@@ -2,12 +2,16 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.Json;
+using System.Security.Cryptography;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+string encryptionKey = "MyEncryptionKey123";
 // Endpoint för att hämta klassinformation
 app.MapGet("/klass", () =>
 {
@@ -32,7 +36,9 @@ app.MapGet("/elever/{id}", (int id) =>
     {
         new Elev { id = 1, namn = "Jessica", ålder = 15},
         new Elev { id = 2, namn = "Kevin", ålder = 14}
-    }.FirstOrDefault(e => e.id == id);
+    };
+
+    var Elev = elev.FirstOrDefault(e => e.id == id);
 
     if (elev != null)
     {
@@ -55,6 +61,23 @@ app.MapPost("/elever", (HttpContext httpContext) =>
 
 app.Run();
 
+
+    public static class EncryptionHelper
+{ 
+public static string EncryptData(Elev elev, string key)
+    {
+    var jsonString = JsonSerializer.Serialize(elev);
+        byte[] jsonData = Encoding.UTF8.GetBytes(jsonString);
+        using Aes aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = new byte[16]; 
+        using MemoryStream ms = new MemoryStream();
+        using CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
+        cs.Write(jsonData, 0, jsonData.Length);
+        cs.FlushFinalBlock();
+        return Convert.ToBase64String(ms.ToArray());
+    }
+}
 public class Klass
 {
     public int id { get; set; }
